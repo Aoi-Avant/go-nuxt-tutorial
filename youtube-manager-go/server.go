01acb952cb1d20a9530f93b35d8d35babf97772e
hikaru takakura <ytoy.aoi@gmail.com>
2021-01-16 18:05:28 +0900

    merge subtree

---
+++ youtube-manager-go/server.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
