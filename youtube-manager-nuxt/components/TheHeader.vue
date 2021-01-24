c347e6aa8b68eaeb924d2572ce9d84f66397fed1
hikaru takakura <ytoy.aoi@gmail.com>
2021-01-24 21:51:08 +0900

    :sparkles: front側の動画検索機能の実装

--- youtube-manager-nuxt/components/TheHeader.vue
+++ youtube-manager-nuxt/components/TheHeader.vue
@@ -5,5 +5,22 @@
         Youtube Player
       </nuxt-link>
     </div>
+
+    <div class="navbar-menu">
+      <div class="navbar-start">
+        <div class="navbar-item">
+          <form action="/search" method="GET" class="field has-addons">
+            <div class="control">
+              <input name="q" class="input" type="text" placeholder="検索" />
+            </div>
+            <div class="control">
+              <button class="button">
+                Search
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   </nav>
 </template>
